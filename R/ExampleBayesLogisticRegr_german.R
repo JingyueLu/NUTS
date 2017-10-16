2755fc4adf1ea4b8b1168f43b864ff88ebdb5976
marcopalma3 <mapalm3@hotmail.it>
2017-10-16 16:33:53 +0100

    Added Logistic regression and HMC test function for assessing performances.

---
+++ R/ExampleBayesLogisticRegr_german.R
@@ -0,0 +1,68 @@
+#' Bayesian Logistic Regression on German credit data (Hoffman & Gelman, 2011)
+#'
+#' This script contains the logarithm of the posterior distribution of the Bayesian logistic regression model fit to
+#' the German credit data set (1000 observations) available online in the UCI repository. The gradient of the logarithm
+#' of the posterior distribution is also reported. Following Hoffman & Gelman (2011),  all predictors are normalized to
+#' have zero mean #' and unit variance. Each element of the vector of parameters \theta=c(\alpha,\beta) is drawn
+#' indipendently from a normal distribution with zero mean and variance \sigma^2=100.
+
+german <- read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data-numeric",sep = "")
+y <- german[, 25]
+X <- scale(german[,1:24])
+sigma.sq <- 100   ### because sigma.sq <- 100
+alpha.start <- rnorm(1, mean = 0, sd = 10)
+beta.start <- rnorm(24, 0, sd = 10)
+
+# Target <- function(theta){
+#   X <- X
+#   y <- y
+#   sigma.sq <- 100
+#   exp( - sum(log(1 + exp(- y * (theta[1] + X%*%theta[-1])))) - (theta[1]^2 - crossprod(theta[-1])) / (2 * 100))
+# }
+
+# LogTarget2 <- function(theta){
+#   X <- X
+#   y <- y
+#   sigma.sq <- 100
+#   - sum(log(1 + exp(- y * (theta[1] + X%*%theta[-1])))) - (theta[1]^2 - crossprod(theta[-1])) / (2 * 100)
+# }
+
+LogTarget <- function(theta){
+  X.star <- cbind(1,X)
+  y <- y
+  sigma.sq <- 100
+  temp <- X.star*y
+  return(as.numeric(- sum(log(1 + exp(- temp %*% theta))) - crossprod(theta) / (2 * 100)))
+}
+
+GradLogTarget <- function(theta){
+  X.star <- cbind(1,X)
+  y <- y
+  sigma.sq <- 100
+  temp <- X.star*y
+  return(as.vector(t(temp) %*% ((exp(- temp %*% theta)) / (1 + exp(- temp %*% theta))) - theta / sigma.sq))
+}
+
+#all.equal(pracma::grad(LogTarget,aaa),GradLogTarget(aaa)) #Check that the function returns effectively the gradient
+
+# theta.hmc<-hmc(theta.start = c(alpha.start, beta.start), epsilon = 0.039, L = 10, logDensity = LogTarget, M = 1000)
+# ts.plot(theta.hmc[,1])
+# ts.plot(theta.hmc[,2])
+#
+# # Plot a histogram of the first variable, with true density
+# choose.var<-14
+#
+# hist(theta.hmc[, choose.var],freq=FALSE,breaks=50)
+# x = seq(range(theta.hmc[,choose.var])[1],range(theta.hmc[,choose.var])[2],by=0.01)
+# vec.theta<-rep(0,25)
+#
+#
+# target.eval<-function(choose.var,x){
+#   vec.theta<-rnorm(25,sd = 10)
+#   dens<-numeric(length(x))
+#   for (i in 1:length(x)) {
+#     vec.theta[choose.var] <- x[i]
+#     dens[i] <- target(vec.theta)
+#   }
+#   return(dens)
+# }
---
+++ R/HMCTestFunction.R
@@ -0,0 +1,26 @@
+#' Comparing the performances of this version of HMC with respect to HybridMC::hybridMC
+#'
+#' This script contains the example from the function hybridMC (in the package HybridMC), that calls C code.
+#' At the end an overview of the performances of the two functions is presented.
+
+library(HybridMC)
+# ## log density function for double exponential
+logDens <- function(x)   -sum(abs(x))
+dlogDens <- function(x)   -sign(x)
+
+samples.hyb <- hybridMC(y.start=c(1,1), n.samp=2000, logDens=logDens, dLogDens=dlogDens, epsilon=.2, LFsteps=10)
+samples.test <- hmc.nograd(theta.start = c(1,1), epsilon = 0.2, L = 10, logDensity = logDens, dlogDensity=dlogDens, M = 2000)
+
+# For NUTS::hmc.nograd plot the MCMC chains and densities
+ts.plot(samples.test[,1])
+ts.plot(samples.test[,2])
+
+# Plot a histogram of the first variable, with true density
+hist(samples.test[,4],freq=FALSE,breaks=50)
+x = seq(-5,5,len=100)
+lines(x,0.5*dexp(abs(x)),col="red") #true density in red
+# Autocorrelation function
+acf(samples.test)
+
+microbenchmark(hybridMC(y.start = c(1,2,3,4), n.samp=2000, logDens=logDens, dLogDens=dlogDens, epsilon=.2, LFsteps=10),
+               hmc.nograd(theta.start = c(1,2,3,4), M = 2000, logDensity = logDens, dlogDensity=dlogDens, epsilon = 0.2, L = 10))
