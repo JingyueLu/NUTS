3db0fb055b540ef5a13d1b09fe32821d48ab5527
jodie0399 <jingyue.lu@spc.ox.ac.uk>
2017-10-17 22:06:20 +0100

    Added HmcDualMod script and modified normalTest to included plots for HmcDualMod

---
+++ R/HmcDualMod.R
@@ -0,0 +1,64 @@
+#' Hamiltonian Monte Carlo with Dual Averaging and random trajectory length
+#'
+#' The function is modified version of Hamiltonian Monte Carlo with Dual Averaging. Based on the post by Radford Neal,
+#' Hamiltonian dynamics can be periodic for variables. It is possible for the period for a variabl to match its trajectory
+#' length, resulting the ending point close to the starting point. To address this issue, Neal proposes to randomly vary the
+#' trajectory length over some moderate range. Therefore, one modification is made:
+#' the previous code: Lm<-max(1,round(lambda/epsilon)) is changed to
+#' the new code: Lm<-max(1,round((0.9 + runif(1)/5)*lambda/epsilon))
+#'
+#' @param theta0 a p-dimensional vector with the initial value for each parameter.
+#' @param delta 	the desired average acceptance probability
+#' @param lambda trajectory length for leapfrog
+#' @param L a callable function that returns a 2-dimensional vector whose first element is the
+#' logarithm of the density of the input and the second is the gradient of the logarithm
+#' of the density evaluated at the input.
+#' @param M an integer specifying the number of iterations.
+#' @param Madapt an integer specifying the number of iterations of the warmup phase.
+#'
+#' @return This function returns a matrix which m-th row is a sample from the joint density.
+#'
+HmcDualMod <- function(theta0, delta, lambda, L, M, Madapt) {
+  len <- length(theta0)
+  outcome <-  matrix(theta0 , nrow = Madapt + M, ncol = len , byrow = T)
+  alpha.vec <- rep(NA,Madapt + M)
+
+  # Find a reasonable initial value of epsilon using heuristic
+  g(log.temp, grad0) %=% L(theta0)
+  epsilon =  FindReasonableEpsilon(theta0, log.temp, L)
+
+  # Setting up other parameters for dual averaging
+  mu <- log(10*epsilon)
+  epsilon.bar <- 1
+  H.bar <- 0
+  gamma <- 0.05
+  t0 <- 10
+  kappa <- 0.75
+
+  for (m in 2:(Madapt+M)) {
+    r0 <- rnorm(len,  0, sd = 1)
+    outcome[m, ] <- outcome[m-1, ]
+    proposal <- list(theta.tilde = outcome[m-1, ], r.tilde = r0)
+    Lm<-max(1,round((0.9 + runif(1)/5)*lambda/epsilon))
+    for (i in 1:Lm)  {
+      g(proposal$theta.tilde, proposal$r.tilde, trivial)%=%Leapfrog(proposal$theta.tilde, proposal$r.tilde, epsilon, L)
+    }
+    alpha.vec[m] <- min(1,exp(L(proposal$theta.tilde)[[1]] - 1/2 * crossprod(proposal$r.tilde) - L(outcome[m-1, ])[[1]] + 1/2 * crossprod(r0)))
+    if (runif(1) <= alpha.vec[m]){
+      outcome[m,]<-proposal$theta.tilde
+    }
+
+    ###Dual Averaging
+    if (m <= Madapt) {
+      temp <- 1/(m-1+t0)
+      H.bar <- (1 - temp)*H.bar + temp*(delta - alpha.vec[m])
+      epsilon <- exp(mu - sqrt(m-1)/gamma*H.bar)
+      temp <- (m-1)^(-kappa)
+      epsilon.bar <- exp(temp*log(epsilon)+(1-temp)*log(epsilon.bar))
+    } else{
+      epsilon <- epsilon.bar
+    }
+    print(m)
+  }
+  return(list(samples = outcome[(Madapt + 1):(M + Madapt),],alpha = alpha.vec))
+}
--- R/normalTest.R
+++ R/normalTest.R
@@ -47,6 +47,8 @@ M = B*N
 Madapt = 2000
 delta =0.6
 
+####################################################################################
+
 ### Construct sample points by NUTS
 
 # L returns log posterior density and its gradient
@@ -61,7 +63,6 @@ theta0 <- rnorm(30)
 
 g(sampleNuts, acceprate, epsilonNor, height) %=% NutsDual(theta0, delta, L, M, Madapt)
 
-
 ### Construct independent sample points using build-in normal functions
 sampleInd = matrix(0, nrow = M, ncol = length(theta0));
 for (i in 1:M){
@@ -72,22 +73,102 @@ for (i in 1:M){
 ### Construct sample point by HMC
 
 ## trajectory lengths is 100
+HMD1st <- Sys.time()
+g(sampleHMD1, alpha1) %=% HmcDual(theta0, delta, 100, L, M, Madapt)
+HMD1ed <- Sys.time()
+HMD1time<- difftime(HMD1ed,HMD1st)
 ## trajectory lengths is 170
+HMD2st <- Sys.time()
+g(sampleHMD2, alpha2) %=% HmcDual(theta0, delta, 170, L, M, Madapt)
+HMD2ed <- Sys.time()
+HMD2time<- difftime(HMD2ed,HMD2st)
 ## trajectory lengths is 200
+HMD3st <- Sys.time()
+g(sampleHMD3, alpha3) %=% HmcDual(theta0, delta, 200, L, M, Madapt)
+HMD3ed <- Sys.time()
+HMD3time<- difftime(HMD3ed,HMD3st)
 
+### Construct sample point by HMC Modified
+
+## trajectory lengths is 100
+HMD1stM <- Sys.time()
+g(sampleHMD1mod, alpha1mod) %=% HmcDualMod(theta0, delta, 100, L, M, Madapt)
+HMD1edM <- Sys.time()
+HMD1timeMod<- difftime(HMD1edM,HMD1stM)
+## trajectory lengths is 170
+HMD2stM <- Sys.time()
+g(sampleHMD2mod, alpha2mod) %=% HmcDualMod(theta0, delta, 170, L, M, Madapt)
+HMD2edM <- Sys.time()
+HMD2timeMod<- difftime(HMD2edM,HMD2stM)
+## trajectory lengths is 200
+HMD3stM <- Sys.time()
+g(sampleHMD3mod, alpha3mod) %=% HmcDualMod(theta0, delta, 200, L, M, Madapt)
+HMD3edM <- Sys.time()
+HMD3timeMod<- difftime(HMD3edM,HMD3stM)
+
+######################################################################
+###Analysing Results
+######################################################################
+
+############# Nuts
 g(muNuts, sdNuts, SubMatrixNuts) %=% analysis(sampleNuts, B,N)
+
+############# Independent
 g(muInd, sdInd, SubMatrixInd) %=% analysis(sampleInd, B,N)
 
+############# HMC with Dual Averaging
+## HMD with trajectory 100
+g(muHMD1, sdHMD1, SubMatrixHMD1) %=% analysis(sampleHMD1, B,N)
+## HMD with trajectory 170
+g(muHMD2, sdHMD2, SubMatrixHMD2) %=% analysis(sampleHMD2, B,N)
+## ## HMD with trajectory 200
+g(muHMD3, sdHMD3, SubMatrixHMD3) %=% analysis(sampleHMD3, B,N)
+
+############## HMC with Dual Averaging and random trajectory
+## HMD with trajectory 100
+g(muHMD1mod, sdHMD1mod, SubMatrixHMD1mod) %=% analysis(sampleHMD1mod, B,N)
+## HMD with trajectory 170
+g(muHMD2mod, sdHMD2mod, SubMatrixHMD2mod) %=% analysis(sampleHMD2mod, B,N)
+## ## HMD with trajectory 200
+g(muHMD3mod, sdHMD3mod, SubMatrixHMD3mod) %=% analysis(sampleHMD3mod, B,N)
+
+
+######################################################################
 ### Plot the results
+######################################################################
+
 x <- seq(1,30,by=1)
 
-## variance of mean estimate (NUTS & Independent)
+
+########### NUTS & INDEPENDENT
+
+# variance of mean estimate (NUTS & Independent)
 plot(x, SubMatrixNuts[,1],log="y", xlab ="NUTS(red) / Independent(black)", ylab="variance of mean estimate", col="red",pch=16)
 points(x,SubMatrixInd[,1], log="y", col="black", pch=16)
 
-## variance of variance estimate (NUTS & Independent)
+# variance of variance estimate (NUTS & Independent)
 plot(x, SubMatrixNuts[,2],log="y", xlab ="NUTS(red) / Independent(black)", ylab="variance of variance estimate", col="red",pch=16)
 points(x,SubMatrixInd[,2], log="y", col="black", pch=16)
 
+########### HMC Dual (trajectory = 100, 170, 200)
 
+## variance of mean estimate ()
+plot(x, SubMatrixHMD1[,1],log="y", xlab ="trajectory length 100(red); 170(green); 200(blue)", ylab="variance of mean estimate", col="red",pch=16)
+points(x,SubMatrixHMD2[,1], log="y", col="green", pch=16)
+points(x,SubMatrixHMD3[,1], log="y", col="blue", pch=16)
 
+## variance of variance estimate (NUTS & Independent)
+plot(x, SubMatrixHMD1[,2],log="y", xlab ="trajectory length 100(red); 170(green); 200(blue)", ylab="variance of mean estimate", col="red",pch=16)
+points(x,SubMatrixHMD2[,2], log="y", col="green", pch=16)
+points(x,SubMatrixHMD3[,2], log="y", col="blue", pch=16)
+
+########### HMC Dual with random trajectory (trajectory = 100, 170, 200)
+## variance of mean estimate ()
+plot(x, SubMatrixHMD1mod[,1],log="y", xlab ="trajectory length 100(red); 170(green); 200(blue)", ylab="variance of mean estimate", col="red",pch=16)
+points(x,SubMatrixHMD2mod[,1], log="y", col="green", pch=16)
+points(x,SubMatrixHMD3mod[,1], log="y", col="blue", pch=16)
+
+## variance of variance estimate (NUTS & Independent)
+plot(x, SubMatrixHMD1mod[,2],log="y", xlab ="trajectory length 100(red); 170(green); 200(blue)", ylab="variance of mean estimate", col="red",pch=16)
+points(x,SubMatrixHMD2mod[,2], log="y", col="green", pch=16)
+points(x,SubMatrixHMD3mod[,2], log="y", col="blue", pch=16)
