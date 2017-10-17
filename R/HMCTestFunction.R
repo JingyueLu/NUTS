4bc6292077d699de1f0ecc9b6bcd1f42b585ed03
jodie0399 <jingyue.lu@spc.ox.ac.uk>
2017-10-17 11:59:54 +0100

    added plots for NutsDual

--- R/HMCTestFunction.R
+++ R/HMCTestFunction.R
@@ -8,12 +8,20 @@ library(HybridMC)
 logDens <- function(x)   -sum(abs(x))
 dlogDens <- function(x)   -sign(x)
 
+L <- function(x) {
+  return(list(-sum(abs(x)), -sign(x)))
+}
+
 samples.hyb <- hybridMC(y.start=c(1,1), n.samp=2000, logDens=logDens, dLogDens=dlogDens, epsilon=.2, LFsteps=10)
 samples.test <- hmc.nograd(theta.start = c(1,1), epsilon = 0.2, L = 10, logDensity = logDens, dlogDensity=dlogDens, M = 2000)
+samples.nuts <- NutsDual(c(1,1),0.6, L, 2000, 500)
 
 # For NUTS::hmc.nograd plot the MCMC chains and densities
 ts.plot(samples.test[,1])
 ts.plot(samples.test[,2])
+
+ts.plot(samples.nuts[,1])
+ts.plot(samples.nuts[,2])
 
 # Plot a histogram of the first variable, with true density
 hist(samples.test[,4],freq=FALSE,breaks=50)
@@ -21,6 +29,13 @@ x = seq(-5,5,len=100)
 lines(x,0.5*dexp(abs(x)),col="red") #true density in red
 # Autocorrelation function
 acf(samples.test)
+
+hist(samples.nuts[,2],freq=FALSE,breaks=50)
+x = seq(-5,5,len=100)
+lines(x,0.5*dexp(abs(x)),col="red") #true density in red
+# Autocorrelation function
+acf(samples.nuts)
+
 
 microbenchmark(hybridMC(y.start = c(1,2,3,4), n.samp=2000, logDens=logDens, dLogDens=dlogDens, epsilon=.2, LFsteps=10),
                hmc.nograd(theta.start = c(1,2,3,4), M = 2000, logDensity = logDens, dlogDensity=dlogDens, epsilon = 0.2, L = 10))
