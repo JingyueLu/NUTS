34826d87d6e159e8c8d8de177259cc1733362610
marcopalma3 <mapalm3@hotmail.it>
2017-10-18 02:37:21 +0100

    Nev version of vanilla HMC: made close to the other functions

--- R/hmc.R
+++ R/hmc.R
@@ -1,70 +1,111 @@
-#' Hamiltonian Monte Carlo
+#' %=%
 #'
-#' @param theta.start a p-dimensional vector with the initial value for each parameter.
+#' The function is copied from "https://strugglingthroughproblems.wordpress.com/2010/08/27/matlab-style-multiple-assignment-in%C2%A0r/" and is used for multiple assignment
+#' @param l		the list on the left-hand side
+#' @param r 	the list on the right-hand side
+#'
+#' @example
+#' g(a,b) = list(c(3,5,6),4)
+#'
+'%=%' = function(l, r, ...) UseMethod('%=%')
+
+#' %=%,lbunch
+#'
+#' The function is copied from "https://strugglingthroughproblems.wordpress.com/2010/08/27/matlab-style-multiple-assignment-in%C2%A0r/"
+#' It is a specific implementation for the generic %=%
+#' @param l		the list on the left-hand side
+#' @param r 	the list on the right-hand side
+#'
+'%=%.lbunch' = function(l, r, ...) {
+  Envir = as.environment(-1)
+
+  if (length(r) > length(l))
+    warning("RHS has more args than LHS. Only first", length(l), "used.")
+
+  if (length(l) > length(r))  {
+    warning("LHS has more args than RHS. RHS will be repeated.")
+    r <- extendToMatch(r, l)
+  }
+
+  for (II in 1:length(l)) {
+    do.call('<-', list(l[[II]], r[[II]]), envir=Envir)
+  }
+}
+
+
+#' g
+#'
+#' The function is copied from "https://strugglingthroughproblems.wordpress.com/2010/08/27/matlab-style-multiple-assignment-in%C2%A0r/".
+#' This function grabs '...' part without evaluating it (so pass by name)
+#'
+#'
+g = function(...) {
+  List = as.list(substitute(list(...)))[-1L]
+  class(List) = 'lbunch'
+  return(List)
+}
+
+
+#' Leapfrog
+#'
+#' This function performs a leapfrog step. This function is a modified version of Leapfrog in the paper.
+#' It returns extra values: log posterior value and gradient of log posterior at the new position theta.tilde
+#'
+#' @param theta		starting position
+#' @param r 	starting momentum
+#' @param epsilon 	step size
+#' @param L 	callable function: returns the value of log posterior and the gradient of log posterior probability at given input
+#'
+#' @return the list of updated theta, r and the log posterior value at the updated point
+
+
+# Leapfrog222 <- function(theta, r, epsilon, logDens, dLogDensity) {
+#   r.tilde <- r + dLogDensity(theta) * epsilon / 2
+#   theta.tilde <- theta + epsilon * r.tilde
+#   r.tilde <- r.tilde + dLogDensity(theta.tilde) * epsilon / 2
+#   return(list(theta.tilde = theta.tilde, r.tilde = r.tilde, log.tilde = logDens(theta.tilde) ))
+# }
+
+
+Leapfrog <- function(theta, r, epsilon, L){
+  g(trivial,grad.theta) %=% L(theta)
+  r.tilde <- r+ 0.5*epsilon*grad.theta
+  theta.tilde <- theta + epsilon*r.tilde
+  g(log.tilde, grad.tilde) %=% L(theta.tilde)
+  r.tilde <- r.tilde + 0.5*epsilon*grad.tilde
+  return(list(theta.tilde, r.tilde, log.tilde))
+}
+
+######################################################################
+######################################################################
+######################################################################
+
+#' Hmc
+#'
+#' This function performs the Hamiltonian Monte Carlo.
+#'
+#' @param theta0 a p-dimensional vector with the initial value for each parameter.
 #' @param epsilon a non-negative value specifying the leapfrog step size.
-#' @param L an integer number specifying the length of the leapfrog step.
-#' @param logDensity a function that computes the logarithm of the joint density of the variables.
+#' @param leap.nsteps an integer number specifying the number of leapfrog steps.
+#' @param L callable function: returns the value of log posterior and the gradient of log posterior probability at given input.
 #' @param M an integer specifying the number of iterations.
 #'
-#' @return This function returns a matrix which m-th row is a sample from the joint density.
+#' @return This function returns a matrix whose m-th row is a sample from the joint density.
 
-hmc.nograd <- function(theta.start, epsilon, L, logDensity, dlogDensity, M) {
-
-  leapfrog <- function(theta, r, epsilon, dlogDensity) {
-    r.tilde <- r + dlogDensity(theta) * epsilon / 2
-    theta.tilde <- theta + epsilon * r.tilde
-    r.tilde <- r.tilde + dlogDensity(theta.tilde) * epsilon / 2
-    return(list(theta.tilde = theta.tilde, r.tilde = r.tilde))
-  }
-
-  p <- length(theta.start)
-  outcome <- list(theta = matrix(theta.start, nrow = M+1, ncol = p, byrow = T), r = matrix(NA, nrow = M+1, ncol = p))##, dimnames[[2]] = paste("Iteration ",1:M))
+Hmc <- function(theta0, epsilon, leap.nsteps, L, M) {
+  len <- length(theta0)
+  outcome <- matrix(theta0, nrow = M+1, ncol = len, byrow = T)
   for (m in 2:(M+1)) {
-    r0 <- rnorm(p,  0, sd = 1)
-    outcome$theta[m, ] <- outcome$theta[m-1, ]
-    proposal <- list(theta.tilde = outcome$theta[m-1, ], r.tilde = r0)
-    for (i in 1:L)  {
-      proposal <- leapfrog(proposal$theta.tilde, proposal$r.tilde, epsilon, dlogDensity)
+    r0 <- rnorm(len,  0, sd = 1)
+    outcome[m, ] <- outcome[m-1, ]
+    proposal <- list(theta.tilde = outcome[m-1, ], r.tilde = r0)
+    for (i in 1:leap.nsteps)  {
+      g(proposal$theta.tilde, proposal$r.tilde, trivial) %=% Leapfrog(proposal$theta.tilde, proposal$r.tilde, epsilon, L)
     }
-    alpha <- min(1, with(proposal,exp(logDensity(theta.tilde) - 1/2 * crossprod(r.tilde) - logDensity(outcome$theta[m-1, ]) + 1/2 * crossprod(r0))))
+    alpha <- min(1, exp(L(proposal$theta.tilde)[[1]] - 1/2 * crossprod(proposal$r.tilde) - L(outcome[m-1, ])[[1]] + 1/2 * crossprod(r0)))
     if (runif(1) <= alpha){
-      outcome$theta[m,] <- proposal$theta.tilde
-      outcome$r[m, ] <- - proposal$r.tilde
+      outcome[m,] <- proposal$theta.tilde
     }
   }
-  return(outcome$theta[-1,])
+  return(outcome[-1,])
 }
-
-
-
-
-
-
-hmc <- function(theta.start, epsilon, L=5, logDensity, M) {
-
-  leapfrog <- function(theta, r, epsilon, logDensity) {
-    require(pracma)
-    r.tilde <- r + grad(logDensity,theta) * epsilon / 2
-    theta.tilde <- theta + epsilon * r.tilde
-    r.tilde <- r.tilde + grad(logDensity,theta.tilde) * epsilon / 2
-    return(list(theta.tilde = theta.tilde, r.tilde = r.tilde, grad.logDens.tilde = grad(logDensity,theta.tilde)))
-  }
-
-  p <- length(theta.start)
-  outcome <- list(theta = matrix(theta.start, nrow = M+1, ncol = p, byrow = T), r = matrix(NA, nrow = M+1, ncol = p))##, dimnames[[2]] = paste("Iteration ",1:M))
-  for (m in 2:(M+1)) {
-    r0 <- rnorm(p,  0, sd = 1)
-    outcome$theta[m, ] <- outcome$theta[m-1, ]
-    proposal <- list(theta.tilde = outcome$theta[m-1, ], r.tilde = r0)
-    for (i in 1:L)  {
-      proposal <- leapfrog(proposal$theta.tilde, proposal$r.tilde, epsilon, logDensity)[1:2]
-    }
-    alpha <- min(1, with(proposal,exp(logDensity(theta.tilde) - 1/2 * crossprod(r.tilde) - logDensity(outcome$theta[m-1, ]) + 1/2 * crossprod(r0))))
-    if (runif(1) <= alpha){
-      outcome$theta[m,] <- proposal$theta.tilde
-      outcome$r[m, ] <- - proposal$r.tilde
-    }
-  }
-  return(outcome$theta[-1,])
-}
-
