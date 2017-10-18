18a746a4c0aba97526ae5177deb4e866a3f0df76
marcopalma3 <mapalm3@hotmail.it>
2017-10-18 02:38:12 +0100

    Removed older code

--- R/Plot1test.R
+++ R/Plot1test.R
@@ -1,5 +1,3 @@
-###PLOTS: average acceptance rates
-
 ##Normal Case
 L <- function(theta){
   cov = c(1,4)
@@ -10,34 +8,38 @@ L <- function(theta){
 
 theta0 <- rnorm(2)
 r <- rnorm(2)
-# Testing Find epsilon
 
 
-M = 5000
-Madapt = 1000
+###Code above is no longer used
 
-g(samples, acceprate, epsilonNor, height) %=% NutsDual(theta0, 0.6, L, M, Madapt)
-
-deltaSeq <-seq(0.25, 0.95, by= 0.10 )
-NumSeed = 3
-DualAvMatrix = matrix(0, nrow =length(deltaSeq)*NumSeed, ncol=4)
-EssMatrix = matrix(0, nrow =length(deltaSeq)*NumSeed, ncol=4)
-
-for (i in 1:length(deltaSeq) ){
-  for (j in 1:NumSeed){
-    set.seed(j)
-    g(samples, acceprate, epsilonNor, steps) %=% NutsDual(theta0, deltaSeq[i], L, M, Madapt)
-    DualAvMatrix[(NumSeed*(i-1)+j),] <-  c(j, deltaSeq[i], (mean(acceprate)-deltaSeq[i]),9999)
-    EssMatrix[(NumSeed*(i-1)+j),] <- c(j, deltaSeq[i], multiESS(samples),9999)
-    print(NumSeed*(i-1)+j)
-  }
-}
-
-essbar <- c()
-for (i in 1:length(deltaSeq)){
-  a <-EssMatrix[((i-1)*NumSeed+1):((i-1)*NumSeed+NumSeed), 3]
-  essbar <- c(essbar, mean(a))
-}
-plot(deltaSeq, essbar)
-
-#EssMatrix = matrix(0, nrow =150, ncol=4)
+# # Testing Find epsilon
+#
+#
+# M = 5000
+# Madapt = 1000
+#
+# g(samples, acceprate, epsilonNor, height) %=% NutsDual(theta0, 0.6, L, M, Madapt)
+#
+# deltaSeq <-seq(0.25, 0.95, by= 0.10 )
+# NumSeed = 3
+# DualAvMatrix = matrix(0, nrow =length(deltaSeq)*NumSeed, ncol=4)
+# EssMatrix = matrix(0, nrow =length(deltaSeq)*NumSeed, ncol=4)
+#
+# for (i in 1:length(deltaSeq) ){
+#   for (j in 1:NumSeed){
+#     set.seed(j)
+#     g(samples, acceprate, epsilonNor, steps) %=% NutsDual(theta0, deltaSeq[i], L, M, Madapt)
+#     DualAvMatrix[(NumSeed*(i-1)+j),] <-  c(j, deltaSeq[i], (mean(acceprate)-deltaSeq[i]),9999)
+#     EssMatrix[(NumSeed*(i-1)+j),] <- c(j, deltaSeq[i], multiESS(samples),9999)
+#     print(NumSeed*(i-1)+j)
+#   }
+# }
+#
+# essbar <- c()
+# for (i in 1:length(deltaSeq)){
+#   a <-EssMatrix[((i-1)*NumSeed+1):((i-1)*NumSeed+NumSeed), 3]
+#   essbar <- c(essbar, mean(a))
+# }
+# plot(deltaSeq, essbar)
+#
+# #EssMatrix = matrix(0, nrow =150, ncol=4)
